2c090951686cad7b04c592281fa142e654eaf630
dgs65 <52545571+dgs65@users.noreply.github.com>
2019-10-16 20:43:44 -0400

    Added Menu and Reset
    Reset button currently works press b to reset
    Parameter menu is there, but I couldn't get it hooked up properly

--- V1/CowDuction/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
+++ V1/CowDuction/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/FirstPersonController.cs
@@ -55,12 +55,14 @@ namespace UnityStandardAssets.Characters.FirstPerson
             m_Jumping = false;
             m_AudioSource = GetComponent<AudioSource>();
 			m_MouseLook.Init(transform , m_Camera.transform);
+
         }
 
 
         // Update is called once per frame
         private void Update()
         {
+
             RotateView();
             // the jump state needs to read here to make sure it is not missed
             if (!m_Jump)
