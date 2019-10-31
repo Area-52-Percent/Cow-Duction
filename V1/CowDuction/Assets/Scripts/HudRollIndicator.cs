2e29f9758da68ea71702c54697049af5079a8939
Vincent Nguyen <vtn28@drexel.edu>
2019-10-31 17:49:58 -0400

    update cow model
    - Use latest model for cow prefab
    - Delete old versions

--- V1/CowDuction/Assets/Scripts/HudRollIndicator.cs
+++ V1/CowDuction/Assets/Scripts/HudRollIndicator.cs
@@ -5,6 +5,7 @@ public class HudRollIndicator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localEulerAngles = Vector3.forward * -Camera.main.transform.eulerAngles.z;
+        if (Camera.main && Camera.main.enabled)
+            transform.localEulerAngles = Vector3.forward * -Camera.main.transform.eulerAngles.z;
     }
 }
