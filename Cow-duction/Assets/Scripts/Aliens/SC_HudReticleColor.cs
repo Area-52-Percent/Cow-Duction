dbeccba93c2b1a5a3e035792c1d8810f7d78feb6
Kadeo13 <kadeo13@gmail.com>
2020-01-17 02:14:46 -0500

    reticle works just not correctly

---
+++ Cow-duction/Assets/Scripts/Aliens/SC_HudReticleColor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class SC_HudReticleColor : MonoBehaviour
+{
+    private Color color;
+    private Color initColor;
+    private Image image;
+    public Renderer rend;
+    public Camera cam;
+    public Transform reticle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        image = GetComponent<Image>();
+        rend = GetComponent<Renderer>();
+        Debug.Log(rend);
+        initColor = image.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            Debug.Log("in if");
+            if (hit.collider)
+            {
+                image.color = Color.green;
+            }
+        }
+        else
+        {
+            image.color = Color.white;
+        }
+    }
+}
