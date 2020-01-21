7d8404521cc7e680b9a058c48d2f9318c3caa537
dgs65 <52545571+dgs65@users.noreply.github.com>
2020-01-21 14:28:33 -0500

    Added Barn Destruction Script

---
+++ Cow-duction/Assets/Scripts/Environment/BarnDestruction.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarnDestruction : MonoBehaviour
+{
+    public GameObject DestroyedPrefab;
+
+    //void OnCollisionEnter(Collision col)
+    void Start()
+    {
+        //if (col.gameObject.name == "AlienProjectile")
+        //{
+        Debug.Log("L");
+            Instantiate(DestroyedPrefab, transform.position, transform.rotation);
+            Destroy(gameObject);
+        //}
+    }
+}
