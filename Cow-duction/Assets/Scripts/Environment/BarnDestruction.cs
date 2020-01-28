543154e38d494966e928ca16fa0bc401fcaaddbb
dgs65 <dgs65@drexel.edu>
2020-01-28 13:20:52 -0500

    Added in barn repairing/Merged in Devin Branch

--- Cow-duction/Assets/Scripts/Environment/BarnDestruction.cs
+++ Cow-duction/Assets/Scripts/Environment/BarnDestruction.cs
@@ -4,16 +4,87 @@ using UnityEngine;
 
 public class BarnDestruction : MonoBehaviour
 {
-    public GameObject DestroyedPrefab;
-
-    //void OnCollisionEnter(Collision col)
+    public GameObject myObject;
+    public GameObject DestroyedObjectPrefab;
+    public Mesh destroyedMesh;
+    private Mesh defaultMesh;
+    private bool isDestroyed;
+    private bool isRepairing;
+    public float timeToRepair;
+    private float curTime;
     void Start()
     {
-        //if (col.gameObject.name == "AlienProjectile")
-        //{
-        Debug.Log("L");
-            Instantiate(DestroyedPrefab, transform.position, transform.rotation);
-            Destroy(gameObject);
-        //}
+        isDestroyed = false;
+        isRepairing = false;
+        defaultMesh = myObject.GetComponent<MeshFilter>().mesh;
+        curTime = 0;
+    }
+    void Update()
+    {
+        if(isRepairing)
+        {
+            if (curTime > timeToRepair)
+            {
+                Destroy(gameObject.transform.Find("destroyedObject"));
+                myObject.SetActive(true);
+                curTime = 0;
+                isRepairing = false;
+                isDestroyed = false;
+            }
+            else
+                curTime += Time.deltaTime;
+        }
+    }
+    //**Mesh Swapping Method**
+    //Usable for a static Destroyed Object when physics is NOT wanted on each piece
+    //void OnCollisionEnter(Collision col)
+    //{
+    //    if (!isDestroyed)
+    //    {
+    //        if (col.gameObject.name == "AlienProjectile")
+    //        {
+    //            MeshFilter myMesh = myObject.GetComponent<MeshFilter>();
+    //            myMesh.mesh = destroyedMesh;
+    //        }
+    //    }
+    //    else
+    //    {
+    //        if(col.gameObject.tag == "Farmer")
+    //        {
+    //            MeshFilter myMesh = myObject.GetComponent<MeshFilter>();
+    //            myMesh.mesh = defaultMesh;
+    //        }
+    //    }
+    //}
+    //**New Object Method**
+    //Use when you want to enact physics on the destroyed Object
+    void OnCollisionEnter(Collision col)
+    {
+        if (!isDestroyed)
+        {
+            if (col.gameObject.name == "AlienProjectile")
+            {
+                GameObject destroyedObject = Instantiate(DestroyedObjectPrefab, transform.position, transform.rotation);
+                destroyedObject.name = "destroyedObject";
+                destroyedObject.transform.parent = gameObject.transform;
+                myObject.SetActive(false);
+                isDestroyed = true;
+            }
+        }
+        else
+        {
+            if (col.gameObject.tag == "Farmer")
+            {
+                isRepairing = true;
+            }
+        }
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag == "Farmer")
+        {
+            isRepairing = false;
+            curTime = 0;
+        }
     }
 }
