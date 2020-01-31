8b26bead7a3147dfaae833d781a7d5db08f5f7a6
Kadeo13 <kadeo13@gmail.com>
2020-01-30 22:52:38 -0500

    shooter script gets cow count

---
+++ Cow-duction/Assets/Scripts/Aliens/SC_CowShooter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(SC_SpaceshipMovement))]
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+public class SC_CowShooter : MonoBehaviour
+{
+    private SC_AlienUIManager uiManager;
+    private SC_SpaceshipMovement spaceshipMovement;
+    private float obtainedCows;
+
+    public float shootForce;
+    private void Awake()
+    {
+        spaceshipMovement = GetComponent<SC_SpaceshipMovement>();
+        uiManager = GameObject.FindWithTag("UIManager").GetComponent<SC_AlienUIManager>();
+    }
+
+    public void AddCow()
+    {
+        obtainedCows++;
+    }
+}
