0befb7b20044b047d3b1d9b0d9751e9372736072
Thomas Trimbur <spidy212@msn.com>
2020-02-23 19:18:46 -0500

    Pushing In Progress Cow-shooter update

--- Cow-duction/Assets/Scripts/Aliens/SC_CowShooter.cs
+++ Cow-duction/Assets/Scripts/Aliens/SC_CowShooter.cs
@@ -10,9 +10,15 @@ public class SC_CowShooter : MonoBehaviour
 {
     private SC_AlienUIManager uiManager;
     private float obtainedCows;
-
+    public GameObject crosshair;
+    public GameObject grappleOrigin;
+    public Camera mainCam;
     public float shootForce;
     public GameObject cow;
+    private float captureLength;
+    public float shotSpeed = 3f;
+
+    private GameObject cowClone;
     private void Awake()
     {
         uiManager = GameObject.FindWithTag("UIManager").GetComponent<SC_AlienUIManager>();
@@ -23,12 +29,57 @@ public class SC_CowShooter : MonoBehaviour
         {
             if (obtainedCows >= 0)
             {
-                Instantiate(cow, transform.position, transform.rotation);
+                Vector3 reticlePoint = RectTransformUtility.WorldToScreenPoint(null, crosshair.GetComponent<RectTransform>().position);
+                Ray ray = Camera.main.ScreenPointToRay(reticlePoint);
+                RaycastHit hit;
+
+                // Ignore UFO layer and trigger colliders
+                int layerMask = ~(1 << gameObject.layer);
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask, QueryTriggerInteraction.Ignore))
+                {
+                    captureLength = Vector3.Distance(grappleOrigin.transform.position, hit.transform.position);
+
+                    StartCoroutine(ShootCow(hit));
+                }
+
+
+                /*
+                RaycastHit hit;
+                
+                if (Physics.Raycast(grappleOrigin.transform.position, grappleOrigin.transform.position, out hit))
+                {
+                    if (hit.collider)
+                    {
+                        crosshair.transform.position = mainCam.WorldToScreenPoint(hit.point);
+                        GameObject cowspawn = Instantiate(cow, grappleOrigin.transform.position, grappleOrigin.transform.rotation);
+                        cowspawn.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 8000);
+                    }
+                }
+                */
             }
         }
     }
     public void AddCow()
     {
         obtainedCows++;
+    }
+
+    private IEnumerator ShootCow(RaycastHit hit)
+    {
+        Vector3 grappleHitPoint = hit.point;
+
+        Vector3 reticlePoint = RectTransformUtility.WorldToScreenPoint(null, crosshair.GetComponent<RectTransform>().position);
+        Ray ray = Camera.main.ScreenPointToRay(reticlePoint);
+
+
+        if (cow)
+        {
+            //cowClone = Instantiate(cow, grappleOrigin.transform.position, Quaternion.identity);
+            cowClone = Instantiate(cow, grappleOrigin.transform);
+            Vector3 shotLocation = Vector3.Lerp(grappleOrigin.transform.position, grappleHitPoint, 1f);
+            cowClone.gameObject.GetComponent<Rigidbody>().AddForce(shotLocation*10);
+        }
+
+        yield return null;
     }
 }
