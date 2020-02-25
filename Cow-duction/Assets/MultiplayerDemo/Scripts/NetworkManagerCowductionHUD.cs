161c7f5863f8d1fad46039fff43980793c352440
Vincent Nguyen <vtn28@drexel.edu>
2020-02-25 17:25:02 -0500

    Add FPS counter

--- Cow-duction/Assets/MultiplayerDemo/Scripts/NetworkManagerCowductionHUD.cs
+++ Cow-duction/Assets/MultiplayerDemo/Scripts/NetworkManagerCowductionHUD.cs
@@ -17,11 +17,14 @@ public class NetworkManagerCowductionHUD : MonoBehaviour
     /// Whether to show the default control HUD at runtime.
     /// </summary>
     public bool showGUI = true;
+    public bool showFPS = true;
 
     [Header("Custom GUI")]
     public GameObject mainMenu;
     public InputField networkAddressField;
     public GameObject pauseMenu;
+
+    private float deltaTime = 0f;
 
     void Awake()
     {
@@ -34,6 +37,22 @@ public class NetworkManagerCowductionHUD : MonoBehaviour
 
     void OnGUI()
     {
+        if (showFPS)
+        {
+            int w = Screen.width, h = Screen.height;
+    
+            GUIStyle style = new GUIStyle();
+    
+            Rect rect = new Rect(0, 0, w, h * 4 / 100);
+            style.alignment = TextAnchor.UpperLeft;
+            style.fontSize = h * 4 / 100;
+            style.normal.textColor = new Color (0.0f, 0.0f, 0.5f, 1.0f);
+            float msec = deltaTime * 1000.0f;
+            float fps = 1.0f / deltaTime;
+            string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
+            GUI.Label(rect, text, style);
+        }
+
         if (!showGUI)
             return;
 
@@ -122,6 +141,8 @@ public class NetworkManagerCowductionHUD : MonoBehaviour
 
     void Update()
     {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+
         if (!NetworkClient.isConnected && !NetworkServer.active)
         {
             if (!NetworkClient.active)
