6da67d0eb917657de997a3f36aa4c4dda44fa464
BuildTools <unconfigured@null.spigotmc.org>
2020-03-02 01:33:44 -0500

    Attempting another revert, missing content of scripts

--- Cow-duction/Assets/Scripts/Aliens/SC_CowShooter.cs
+++ Cow-duction/Assets/Scripts/Aliens/SC_CowShooter.cs
@@ -12,8 +12,13 @@ public class SC_CowShooter : MonoBehaviour
     private SC_SpaceshipMovement spaceshipMovement;
     private float obtainedCows;
 
+<<<<<<< HEAD
     public float shootForce;
     public GameObject cow;
+=======
+    private GameObject cowClone;
+    private GameObject fullCow;
+>>>>>>> parent of 137dbb4... Current Update with teleporting and partial cow-shooter mechanics
     private void Awake()
     {
         spaceshipMovement = GetComponent<SC_SpaceshipMovement>();
@@ -24,11 +29,51 @@ public class SC_CowShooter : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(1))
         {
+<<<<<<< HEAD
             Debug.Log(obtainedCows);
+=======
+            if (obtainedCows >= 0)
+            {
+                Vector3 reticlePoint = RectTransformUtility.WorldToScreenPoint(null, crosshair.GetComponent<RectTransform>().position);
+                Ray ray = Camera.main.ScreenPointToRay(reticlePoint);
+                RaycastHit hit;
+
+                int layerMask = ~(1 << gameObject.layer);
+                if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask, QueryTriggerInteraction.Ignore))
+                {
+                    StartCoroutine(ShootCow(hit));
+                }
+            }
+>>>>>>> parent of 137dbb4... Current Update with teleporting and partial cow-shooter mechanics
         }
     }
     public void AddCow()
     {
         obtainedCows++;
+<<<<<<< HEAD
+=======
+    }
+
+    private IEnumerator ShootCow(RaycastHit hit)
+    {
+        Vector3 grappleHitPoint = hit.point;
+
+        Vector3 reticlePoint = RectTransformUtility.WorldToScreenPoint(null, crosshair.GetComponent<RectTransform>().position);
+        Ray ray = Camera.main.ScreenPointToRay(reticlePoint);
+
+        if (dehydratedCow)
+        {
+            cowClone = Instantiate(dehydratedCow, grappleOrigin.transform.position, ufo.transform.rotation);
+            
+            //Vector3 shotLocation = Vector3.Lerp(reticlePoint, grappleHitPoint, 5f);
+            cowClone.gameObject.GetComponent<Rigidbody>().AddForce(ray.direction * shotSpeed, ForceMode.Impulse);
+            //obtainedCows--;
+        }
+
+        yield return new WaitForSeconds(3f);
+        //Transform cowSpawn = cowClone.transform;
+        //fullCow = Instantiate(cow, cowSpawn);
+        //Destroy(cowClone);
+>>>>>>> parent of 137dbb4... Current Update with teleporting and partial cow-shooter mechanics
     }
 }
