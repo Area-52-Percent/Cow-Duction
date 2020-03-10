2e2b0471405f8f6d45b66288666f08044cd5f023
joekez <39750406+joekez@users.noreply.github.com>
2020-03-10 17:05:18 -0400

    Added 4 audio triggers

---
+++ Cow-duction/Assets/Scripts/Environment/AudioTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    public AudioClip SoundToPlay;
+    AudioSource sounds;
+    public bool alreadyPlayed = false;
+    void Start()
+    {
+        sounds = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter()
+    {
+        if (GameObject.FindWithTag("MainCamera"))
+        {
+            if (!alreadyPlayed)
+            {
+                sounds.PlayOneShot(SoundToPlay);
+                alreadyPlayed = true;
+            }
+        }
+    }
+
+}
