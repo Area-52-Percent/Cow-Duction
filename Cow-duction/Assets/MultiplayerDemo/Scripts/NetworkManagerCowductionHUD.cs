494a6c690a7a41bb3893f9c0b36fc24c9b0c974e
Vincent Nguyen <vtn28@drexel.edu>
2020-03-10 10:13:46 -0400

    Add show FPS toggle

--- Cow-duction/Assets/MultiplayerDemo/Scripts/NetworkManagerCowductionHUD.cs
+++ Cow-duction/Assets/MultiplayerDemo/Scripts/NetworkManagerCowductionHUD.cs
@@ -53,7 +53,7 @@ public class NetworkManagerCowductionHUD : MonoBehaviour
             Rect rect = new Rect(0, 0, w, h * 4 / 100);
             style.alignment = TextAnchor.UpperLeft;
             style.fontSize = h * 4 / 100;
-            style.normal.textColor = new Color (0.0f, 0.0f, 0.5f, 1.0f);
+            style.normal.textColor = new Color (0.0f, 1.0f, 0.5f, 1.0f);
             float msec = deltaTime * 1000.0f;
             float fps = 1.0f / deltaTime;
             string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
@@ -200,6 +200,11 @@ public class NetworkManagerCowductionHUD : MonoBehaviour
             pauseMenu.SetActive(true);
             gamemanager.SetMusicVolume(.1f);
         }
+    }
+
+    public void ToggleFpsCounter()
+    {
+        showFPS = !showFPS;
     }
 
     public void DisplayEndScreen(int score, AudioSource ufoAudioSource, AudioClip loseAudio, AudioClip winAudio)
