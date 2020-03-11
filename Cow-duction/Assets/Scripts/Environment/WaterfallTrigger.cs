92b2ea3b3feab32c3b04277148b024b8710d7c61
joekez <39750406+joekez@users.noreply.github.com>
2020-03-11 17:12:27 -0400

    Waterfall+River sound+triggers added

---
+++ Cow-duction/Assets/Scripts/Environment/WaterfallTrigger.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterfallTrigger : MonoBehaviour
+{
+    public AudioClip SoundToPlay;
+    AudioSource sounds;
+    void Start()
+    {
+        sounds = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter()
+    {
+        if (GameObject.FindWithTag("MainCamera"))
+        {
+            sounds.PlayOneShot(SoundToPlay);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        sounds.Stop();
+    }
+
+}
