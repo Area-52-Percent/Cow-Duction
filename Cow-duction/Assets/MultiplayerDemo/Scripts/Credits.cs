4db76adbb6925641aec980649e497139fcf1e520
Acosta <ava48@drexel.edu>
2020-03-11 01:31:23 -0400

    added credit scene

---
+++ Cow-duction/Assets/MultiplayerDemo/Scripts/Credits.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+
+	public string SceneName;
+	
+	public void NextScene()
+	{
+		SceneManager.LoadScene(SceneName);
+	}
+}
